36b7cbc46d9b7f9e84b4ecc7210d06395e54636d
Jensen Turner <jensen.turner@ghostpacer.com>
2021-03-05 17:32:48 -0800

    added led blink on euler move

--- cmd/bno055live/main.go
+++ cmd/bno055live/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/Ghost-Pacer/input-goi2c/pkg/bno055"
+	"gonum.org/v1/gonum/spatial/r3"
 	"gopkg.in/tomb.v2"
 	"log"
 	"os"
@@ -17,6 +18,7 @@ var I2CBus = flag.String("b", "2", "I2C bus")
 var I2CAddr = flag.Int("a", 0x28, "I2C address")
 var RefreshInterval = flag.Duration("r", 8*time.Millisecond, "refresh interval")
 var Verbose = flag.Bool("v", false, "print all sent messages (do not use in production)")
+var Threshold = flag.Float64("t", 2, "Euler angle difference threshold")
 
 func printv(vals ...interface{}) {
 	if *Verbose {
@@ -46,11 +48,17 @@ func run() error {
 	}
 	defer bno.Halt()
 
+	usr3, err := os.OpenFile("/sys/class/leds/beaglebone:green:usr3/brightness", os.O_RDWR|syscall.O_DIRECT, 0600)
+	if err != nil {
+		return err
+	}
+	defer usr3.Close()
+
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
 
 	mainTomb.Go(func() error {
-		return doWork(mainTomb, bno)
+		return doWork(mainTomb, bno, usr3)
 	})
 
 	caughtSignal := <-signalChannel
@@ -59,34 +67,36 @@ func run() error {
 	return mainTomb.Wait()
 }
 
-func doWork(tomb *tomb.Tomb, bno *bno055.Dev) error {
+func doWork(tomb *tomb.Tomb, bno *bno055.Dev, led *os.File) error {
 	ticker := time.NewTicker(*RefreshInterval)
 	defer ticker.Stop()
 
+	var lastEul r3.Vec
 	for {
 		select {
 		case <-tomb.Dying():
 			return nil
 		case <-ticker.C:
-			printv("ticked")
-
-			quat, err := bno.ReadQuat()
+			eulArr, err := bno.ReadEuler()
 			if err != nil {
 				return err
 			}
-			printv("\tgot quat", quat)
 
-			eul, err := bno.ReadEuler()
-			if err != nil {
-				return err
-			}
-			printv("\tgot eul", eul)
+			eul := r3.Vec{eulArr[0], eulArr[1], eulArr[2]}
+			diff := r3.Norm(eul.Sub(lastEul))
+			lastEul = eul
 
-			lin, err := bno.ReadLinearAccel()
-			if err != nil {
-				return err
+			printv("current", eul, "last", lastEul, "diff", diff)
+
+			if diff > *Threshold {
+				if _, err := led.WriteString("1"); err != nil {
+					return err
+				}
+				time.Sleep(500 * time.Millisecond)
+				if _, err := led.WriteString("0"); err != nil {
+					return err
+				}
 			}
-			printv("\tgot lin", lin)
 		}
 	}
 }
